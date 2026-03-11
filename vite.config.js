2db61080cc48d60e5fa2ecd6d44b1d5061d42430
Isosh <412956@niuitmo.ru>
2026-03-11 10:10:46 +0300

    fix: build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	base: "/ceramic-web",
 	build: {
 		rollupOptions: {
 			input: {
